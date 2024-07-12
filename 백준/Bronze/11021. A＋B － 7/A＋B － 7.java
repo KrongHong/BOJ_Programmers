cb3fbee6bbc36f7256717cfcb96c644a473e9b6f
KrongHong <160224630+KrongHong@users.noreply.github.com>
2024-07-12 14:47:17 +0900

    [Bronze V] Title: A+B - 7, Time: 212 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+        int num = cin.nextInt();
+        int sum;
+
+        for (int i = 1; i <= num; i++){
+            int a = cin.nextInt();
+            int b = cin.nextInt();
+            sum = a + b;
+            System.out.println("Case #" + i + ": " + sum);
+        }
+    }
+}

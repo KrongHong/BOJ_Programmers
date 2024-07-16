0d9e0fb5f322528bb39268392621baec9ba4b7f5
KrongHong <160224630+KrongHong@users.noreply.github.com>
2024-07-16 11:17:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 184 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char word = sc.nextLine().charAt(0);
+        int num = (int)word;
+
+        System.out.println(num);
+    }
+}
